a075ad33b2bd28f1c18f97c68f049392535f7cba
jackhowa <johncarverhoward@gmail.com>
2017-02-15 15:43:26 -0600

    Add puts and to_i conversion

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,1 +1,25 @@
-puts "Hello"
+puts "What's the hamster's name?"
+
+name = gets.chomp
+
+puts "How loud is you hamster on a scale of 1-10?"
+
+volume = gets.to_i
+
+puts "What color is its fur?"
+
+fur = gets.chomp
+
+puts "Is the hamster a good candidate for adoption? true or false?"
+
+good_candidate = gets.chomp 
+
+puts "What's its estimated age? If you don't know, say Don't know."
+
+age = gets.chomp
+
+if age = "don't know"
+	age = nil 
+else
+	gets.to_i
+end 
