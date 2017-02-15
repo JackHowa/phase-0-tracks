c26202203e54e4426c27a3ac9feb202eb9c67ca3
jackhowa <johncarverhoward@gmail.com>
2017-02-15 14:54:03 -0600

    Add hello init to ruby hamsters

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,4 +10,10 @@ puts "Oh and what year were you born?"
 
 birth_year = gets.to_i
 
-puts "I, #{name}, am #{age} year old; I was born in #{birth_year}."
+puts "And oh yeah, our cafe has garlic bread. Want some?"
+
+garlic_bread = gets
+
+
+
+# puts "I, #{name}, am #{age} year old; I was born in #{birth_year}."
