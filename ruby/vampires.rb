e48f7db488c57c204880daa63fd55cc28f84d6a6
jackhowa <johncarverhoward@gmail.com>
2017-02-15 14:08:28 -0600

    Include age and birthyear sneaky and some interpolation to see result

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,4 +2,12 @@ puts "What's your name?"
 
 name = gets.chomp 
 
-puts name 
+puts "How old are you?"
+
+age = gets.to_i 
+
+puts "Oh and what year were you born?"
+
+birth_year = gets.to_i
+
+puts "I, #{name}, am #{age} year old; I was born in #{birth_year}."
