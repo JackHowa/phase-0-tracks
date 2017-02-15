d64f20edf70ff35d0494eac04fcf72089074b623
jackhowa <johncarverhoward@gmail.com>
2017-02-15 16:02:25 -0600

    Add coniditonals and boolean conversion

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -10,16 +10,39 @@ puts "What color is its fur?"
 
 fur = gets.chomp
 
-puts "Is the hamster a good candidate for adoption? true or false?"
+puts "Is the hamster a good candidate for adoption? True or false?"
 
-good_candidate = gets.chomp 
+good_candidate = gets.to_bool 
 
-puts "What's its estimated age? If you don't know, say Don't know."
+puts "What's its estimated age? If you don't know, press enter."
 
 age = gets.chomp
 
-if age = "don't know"
+if age == ""
 	age = nil 
 else
 	gets.to_i
 end 
+
+puts "The #{fur} hamster #{name} is about a #{volume} on a scale of 10 for loudness."
+
+if age == nil
+	puts "We don't know how old #{name} is."
+else 
+	puts "The hamster is #{age} years old."
+end 
+
+if good_candidate == true 
+	puts "#{name} is a great candidate for adoption."
+else
+	puts "There may be better choices than #{name} for adoption."
+end 
+
+
+
+
+
+
+
+
+
