1073474d4d1d5ca7ba4e529e29057dc1e5006a0c
jackhowa <johncarverhoward@gmail.com>
2017-02-15 19:31:09 -0600

    Add get data including healthcare variable

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,4 +10,13 @@ puts "Oh and what year were you born?"
 
 birth_year = gets.to_i
 
-puts "I, #{name}, am #{age} year old; I was born in #{birth_year}."
+puts "And oh yeah, our cafe has garlic bread. Want some?"
+
+garlic_bread = gets.chomp
+
+puts "Last, would you like to join our healthcare program?"
+
+healthcare = gets.chomp
+
+
+# puts "I, #{name}, am #{age} year old; I was born in #{birth_year}."
