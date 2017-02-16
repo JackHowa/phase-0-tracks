ab6b81c3eabb9e7fa04a15930bd0587504e32651
jackhowa <johncarverhoward@gmail.com>
2017-02-15 21:59:05 -0600

    Add human age verifier with datetime

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,3 @@
-
-
-
-
-
 # release 1 
 
 # q1
@@ -35,6 +30,14 @@ age = gets.to_i
 puts "Oh and what year were you born?"
 birth_year = gets.to_i
 
+# human age verifier
+if ((age == Time.new.year - birth_year) || (age == Time.new.year - birth_year - 1)) && age < 120
+	age_verify = true 	
+else
+	age_verify = false 
+end 
+p age_verify
+
 # question 3 garlic interest
 puts "Our cafe has garlic bread. Do you ... like garlic bread? (y/n)"
 garlic_bread = gets.chomp
@@ -65,26 +68,48 @@ end
 
 
 =begin 
-p "#{name} is name"
-p "#{age} is how old"
-p "#{birth_year} is birth year"
-p "#{like_garlic} to garlic"
-p "#{healthcare} to healhcare"
 
-alright this seemed to work as planned 
+release 2 
+
+Match the following conditions in the order they're listed. Your program should base its result on the latest condition matched, not the first condition matched. In other words, it's not a "only one condition will apply" sort of scenario. This may mean repeatedly updating a variable as each condition is checked, so that the variable always contains the most precise result.
+->-> this seems more confusing than it is. just do if then method after acquiring info 
+
+
+If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, 
+-> the result is “Probably not a vampire.”
+
+If the employee got their age wrong, and hates garlic bread or waives insurance, 
+-> the result is “Probably a vampire.”
+
+If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, 
+-> the result is “Almost certainly a vampire.”
+
+
+Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” 
+-> is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+* getting tconstant error here 
+
+
+else 
+-> Otherwise, print “Results inconclusive.”
+
+Print the result at the end of the survey.
+
+
+
+
+if 
+
+
+
+
+
+
+elsif (name == “Drake Cula”) || (name == “Tu Fang”) 
+	print "Almost certainly a vampire."
+end 
 
 =end 
-
-
-
-
-
-
-
-
-
-
-
 
 
 
