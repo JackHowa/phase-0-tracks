f82c4ec3c293951aa38cb7cbd357af97f4432b89
jackhowa <johncarverhoward@gmail.com>
2017-02-18 12:49:02 -0600

    Add updated copy of brent including caesar cipher solution without next

--- ruby/caesar_cipher.rb
+++ ruby/caesar_cipher.rb
@@ -1,20 +1,49 @@
 # caesar cipher 
 
+
+
 message = "hellohellooo"
 
-message_length = message.length
 
-current_index = 0
+def encrypt(message)
 
-cipher_word = ""
+	message_length = message.length
 
-until message_length == current_index 
-	cipher_letter = message[current_index].next
-	current_index += 1
-	cipher_word = cipher_word + cipher_letter
-end 
+	current_index = 0
+
+	cipher_word = ""
+
+	until message_length == current_index 
+		cipher_letter = message[current_index].next
+		current_index += 1
+		cipher_word = cipher_word + cipher_letter
+	end 
 
 puts cipher_word
+
+end
+
+encrypt(message)
+
+def decrypt(message)
+
+	cipher_length = cipher_word.length
+
+	current_index = 0
+
+	solution_word = ""
+
+	until cipher_length == current_index 
+		solution_letter = cipher_word[current_index].!next
+		current_index += 1
+		solution_word = solution_word + solution_letter
+	end 
+
+puts cipher_word
+
+end
+
+decrypt(message)
 
 
 
