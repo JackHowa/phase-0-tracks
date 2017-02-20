919b7849d57d53236dfb0ac9e8d06b3fe2cb4c3e
jackhowa <johncarverhoward@gmail.com>
2017-02-20 11:50:30 -0600

    Add method and explanation for method quiz

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,58 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-swapcase
 
-# "zom".<???>
+"zom".insert(1, 'o')
+"zom".gsub(/[o]/, 'oo')
 # => “zoom”
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-insert
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-gsub
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-center
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-upcase
 
-# "the usual".<???>
+"the usual".insert(9," suspects")
 #=> "the usual suspects"
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-insert
 
-# " suspects".<???>
+" suspects".insert(0, "the usual")
 # => "the usual suspects"
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-insert
 
-# "The case of the disappearing last letter".<???>
+
+"The case of the disappearing last letter".chop 
+"The case of the disappearing last letter".slice(0..38)
 # => "The case of the disappearing last lette"
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-chop 
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-slice
 
+"The mystery of the missing first letter".slice(1..39)
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-slice
 
-# "Elementary,    my   dear        Watson!".<???>
+
+"Elementary,    my   dear        Watson!".squeeze 
 # => "Elementary, my dear Watson!"
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-squeeze
 
-# "z".<???>
+
+"z".each_byte {|c| print c, ' ' }
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# 122 is "z" ascii value 
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-each_byte
+
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
+# via http://ruby-doc.org/core-2.4.0/String.html#method-i-count
+
