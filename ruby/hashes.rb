f2a8da2ce8e6f267b1da0a1a882b1a03bf6e9980
jackhowa <johncarverhoward@gmail.com>
2017-02-21 14:25:14 -0600

    Add more streamlined approach including proper symbols as keys

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,20 +7,35 @@ user_info = {
 	experience: nil
 }
 
-# test being able to print out whole hash
-p user_info
-
-
-
 # asking the user for info 
 
 puts "What's your name?"
 
+user_info[:name] = gets.chomp
+
 puts "How old are you?"
+
+user_info[:age] = gets.to_i
 
 puts "How many children do you have?"
 
+user_info[:children] = gets.to_i
+
 puts "What decor theme do you like?"
 
+user_info[:decor_theme] = gets.chomp
+
 puts "Have you ever done interior design? (y/n)"
+
+user_info[:experience] = gets.chomp
+
+#convert the string to a boolean
+if user_info[:experience] == "y"
+	user_info[:experience] = true 
+else user_info[:experience] == "n"
+	user_info[:experience] = false
+end
+
+# test update the hash bool
+p user_info
 
