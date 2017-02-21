6f9e82d6e5eceb3ea530b1187e59e906ba5fd6b8
jackhowa <johncarverhoward@gmail.com>
2017-02-21 14:30:06 -0600

    Add a loop preping for rearranging decor_theme or non

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -36,6 +36,19 @@ else user_info[:experience] == "n"
 	user_info[:experience] = false
 end
 
+# Give the user the opportunity to update a key (no need to loop, once is fine). 
+user_info[:re_enter] = ""
+until user_info[:re_enter] == "none"
+	puts "Did ya' miss anything?"
+	puts "Enter the key you'd like to update (or 'none' to exit)."
+	user_info[:re_enter] = gets.to_sym
+	
+# After all, sometimes users make mistakes! If the designer says "none", skip it. 
+# But if the designer enters "decor_theme" (for example), 
+# your program should ask for a new value and update the :decor_theme key. 
+# (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) 
+# You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+
 # test update the hash bool
 p user_info
 
