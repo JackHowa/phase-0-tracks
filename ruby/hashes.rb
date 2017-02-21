3647539ad481c50e5b4f4c3bb99a0d6e6296d43d
jackhowa <johncarverhoward@gmail.com>
2017-02-21 14:16:10 -0600

    Redesign the hash setup system

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,59 +1,26 @@
-=begin - prompt user to enter data on interior application, including
-		the client's name, age, number of children, decor theme, 
-		and so on (you can choose your own as long as it's a good mix of string, integer, and boolean data).
-
-
-=end 
-
-puts "What's your name"
-user_name = gets.chomp
-
-puts "How old are you?"
-age = gets.to_i
-
-puts "How many children do you have?"
-children = gets.to_i
-
-puts "What decor theme do you like?"
-decor_theme = gets.chomp 
-
-puts "Have you ever done interior design? (y/n)"
-experience = gets.chomp 
-
-#convert the string to a boolean
-if experience == "y"
-	experience = true 
-else experience == "n"
-	experience = false
-end
-
-#convert the key strings to symbols 
+# show the whole hash value
 user_info = {
-	user_name: user_name, 
-	age: age, 
-	children: children,
-	decor_theme: decor_theme,
-	experience: experience
+	name: "", 
+	age: nil, 
+	children: "",
+	decor_theme: "",
+	experience: nil
 }
 
-# print key symbols and corresponding data 
-puts user_info
+# test being able to print out whole hash
+p user_info
 
 
-# update a key 
-puts "Would you like to change your info, like for decor theme, or none?"
-update_key = gets.chomp 
 
-if update_key == "decor theme"
-	puts "What decor theme do you like?"
-	decor_theme = gets.chomp
-elsif update_key == "none"
-	puts "Sounds good, let's move on."
-else 
-	puts "Decor theme or none?"
-end 
+# asking the user for info 
 
-user_info[:decor_theme] = decor_theme
+puts "What's your name?"
 
-puts user_info
+puts "How old are you?"
+
+puts "How many children do you have?"
+
+puts "What decor theme do you like?"
+
+puts "Have you ever done interior design? (y/n)"
 
