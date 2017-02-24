6986205d195915de6f9b83eac7f3b67c65ae0e83
jackhowa <johncarverhoward@gmail.com>
2017-02-24 17:27:56 -0600

    FInish question 3 about getting a whole string to calc using split

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -5,29 +5,61 @@ num2 = nil
 
 operator_string = ""
 
+calc_input = ""
 
+def calculate(num1,operator_string,num2)
 
-def calculate(num1,num2,operator_string)
+	puts "What calculation would you like me to make?"
+	puts ("for instance, 4 + 5")
 
-	num1.send(operator_string,num2)
+	calc_input = gets.chomp 
+
+	calc_elements = calc_input.split
+
+	# num 1 conversion 
+	num1 = calc_elements[0]
+
+	num1 = num1.to_i
+
+	# op conversion 
+	operator_string = calc_elements[1]
+
+	# num 2 conversion 
+	num2 = calc_elements[2]
+
+	num2 = num2.to_i
+
+	# make the equation 
+	output = num1.send(operator_string,num2)
+
+	# print output 
+	puts output
 
 end 
 
-p calculate(2,2,"+")
+calculate(num1,operator_string,num2)
 
-# => 4
 
-p calculate(4,2,"-")
 
-# => 2 
 
-p calculate(5,10,"*")
 
-# => 50
 
-p calculate(4,2, "/") 
 
-# => 2 
+# p calculate(2,2,"+")
+
+# # => 4
+
+# p calculate(4,2,"-")
+
+# # => 2 
+
+# p calculate(5,10,"*")
+
+# # => 50
+
+# p calculate(4,2, "/") 
+
+# # => 2 
 
 
 
