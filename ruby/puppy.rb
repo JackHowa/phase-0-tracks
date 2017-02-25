59599f7ebb99c60a28c6ab18c2f4e0ad0b8574b6
jackhowa <johncarverhoward@gmail.com>
2017-02-25 08:14:52 -0600

    Add a new characteristics and details for puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+=begin 
+
+SPECIES ------------------------
+Puppy 
+
+CHARACTERISTICS ----------------
+Ears: Floppy
+Cute: yes
+Eye count: 2
+Name: Fido, Spot, Duchess 
+
+BEHAVIOR -----------------------
+Play Dead
+Sit 
+
+=end 
