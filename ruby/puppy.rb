cbcfef795a8e0bdd5285603097bcbfd8d53378f5
jackhowa <johncarverhoward@gmail.com>
2017-02-27 12:05:50 -0600

    Add commments for future puppy methods and instances based on instructions

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,16 +1,62 @@
 =begin 
 
-SPECIES ------------------------
+SPECIES ------------------------ # class 
 Puppy 
 
-CHARACTERISTICS ----------------
+CHARACTERISTICS ---------------- # attributes 
 Ears: Floppy
 Cute: yes
 Eye count: 2
 Name: Fido, Spot, Duchess 
 
-BEHAVIOR -----------------------
+BEHAVIOR ----------------------- #  methods 
 Play Dead
 Sit 
 
 =end 
+
+
+# this defines the class 
+class Puppy
+end
+
+
+Puppy.methods
+
+# figure out what methods are even available 
+
+
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+# new is a method
+# puppy is the class 
+# class is like a hash, the blueprint for latter sub-data 
+# duchess, fido, and spot are the instances 
+
+# makes three unique instances of a class 
+
+
+
+spot.class 
+
+# maybe this will print the class 
+# yep that the instance is in 
+
+
+fido.instance_of?(Array)
+# this prints false 
+# because fido is an instance of the class Puppy 
+
+
+fido.instance_of?(Puppy)
+# this prints true 
+
+
+spot.play_dead
+# haven't taught spot to do this quite yet 
+# we haven't defined the method yet 
+
+
