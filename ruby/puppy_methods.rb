b63c1900fb89d9abf6b7828816ea22c1315d6698
jackhowa <johncarverhoward@gmail.com>
2017-02-27 14:14:28 -0600

    Add instance methods for spot in release 0 and printed commands

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,16 +1,39 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+ 	def fetch(toy)
+    	puts "I brought back the #{toy}!"
+    	toy
+  	end
 
-  
+ 	def speak(num) 
+  	num.times {puts "Woof"} 
+	end 
+
+	def roll_over
+		puts "Roll over!"
+	end 
+
+	def dog_years(num)
+		dog_age = num * 7 
+		return dog_age
+	end
+
+	def sit
+		puts "Sit down!"
+	end  
 
 end 
 
 # can call fetch 
 spot = Puppy.new
 
-spot.fetch("bone")
+# spot.fetch("bone")
+
+# spot.speak(3)
+
+# spot.roll_over
+
+# p spot.dog_years(4)
+
+spot.sit
 
