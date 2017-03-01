f2fcfb6924f702b239515e2a309cbe12b0c936e9
jackhowa <johncarverhoward@gmail.com>
2017-03-01 09:47:58 -0600

    Start to set rando age, need more work

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,21 +14,12 @@ class Santa
 		@age = 0 
 	end
 
-	def celebrate_birthday
-		@age += 1
+	def rando_age(age)
+		age = rand(0..140)
+		p age
 	end 
 
-	def get_mad_at(bad_reindeer)
-		@reindeer_ranking.delete(bad_reindeer)
-		@reindeer_ranking << bad_reindeer
-	end
-
 end
-
-
-
-
-Jack = Santa.new("male","German")
 
 santas = []
 
@@ -36,8 +27,9 @@ genders = [["agender", "female", "bigender", "male", "female",
 "gender fluid", "N/A"].sample]
 ethnicities = [["black", "Latino", "white", "Japanese-African", 
 "prefer not to say"].sample]
+
 genders.cycle(100) do 
   santas << Santa.new(genders, ethnicities)
 end
 
-p santas 
+santas.rando_age(@age)
