71ef52966a743ee9c46696a1b3c1f68908e29ca6
jackhowa <johncarverhoward@gmail.com>
2017-03-05 11:36:45 -0600

    Add more pseudocode logic like end game variable

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -5,16 +5,46 @@
 
 # one user enters word 
 # a second user guesses the word with letters 
+# Guesses are limited, and the number of guesses available is related to the length of the word.
 
 # repeated letters not penalized - just dumb 
 
 # prompt one user to enter a word
 	# store that word 
-# UNTIL accumulated letters equals word
+	# measure stored words's length 
+		# double the word's length 
+			# store that as the guess limit 
+# UNTIL guess limit is equal to number of guesses 
 	# prompt a second user to enter a letter 
 		# compare that letter to the stored word 
 			# IF letter is in word 
-				# 
+				# add that letter to the guessed letters list 
+				# show encouraging message
+				# Make the guessed current word include that letter
+			# ELSIF letter is already in guessed letters list 
+				# show all the letters in the word already guessed 
+				# print out already-guessed letters 
+				# subtract one guess from the count (same effect and easier than adding to rest)
+			# ELSE 
+				# show discouraging message 
+				# add one guess to the guess list 
+		# Add one guess to the guess count 
+		# show all of the letters in the word that are present 
+
+	# IF guessed word is equal to word solution
+		# Show encouraging message
+		# Change guess limit end to being true and stop the prompting 
+
+# IF guessed word is not equal to word solution before number of guesses is equal to double solution length 
+	# show the solution
+	# show discouraging message 
+
+
+
+
+
+
+
 
 
 
---
+++ ruby/word_game/word_game_spec.rb
@@ -0,0 +1,8 @@
+require_relative 'todo_list'
+
+describe WordGame do
+
+	let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+
+end 
+
