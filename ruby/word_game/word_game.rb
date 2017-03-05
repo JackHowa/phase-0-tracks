afcfbf4cf6835dedb73a175c70b91c139fe6bbff
jackhowa <johncarverhoward@gmail.com>
2017-03-05 09:17:15 -0600

    Start pseudocoding but not sure about guess count criteria

---
+++ ruby/word_game/word_game.rb
@@ -0,0 +1,20 @@
+# pseudo 
+# 
+
+# handle input and output as far as the user is concerned
+
+# one user enters word 
+# a second user guesses the word with letters 
+
+# repeated letters not penalized - just dumb 
+
+# prompt one user to enter a word
+	# store that word 
+# UNTIL accumulated letters equals word
+	# prompt a second user to enter a letter 
+		# compare that letter to the stored word 
+			# IF letter is in word 
+				# 
+
+
+
