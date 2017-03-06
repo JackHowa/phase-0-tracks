3cb2ae52e964900a195113f5f1a93e442c65dbc4
jackhowa <johncarverhoward@gmail.com>
2017-03-06 17:19:44 -0600

    Start game sequence setup

--- ruby/word_game/word_game_refactor.rb
+++ ruby/word_game/word_game_refactor.rb
@@ -3,7 +3,7 @@ class WordGame
 	attr_reader :solution, :guess_limit, :hangman_line, :guess_count, :letter, :loop 
 
 	def initialize(solution)
-    	@solution = solution
+    	@solution = solution.split("")
     	@guess_limit = @solution.length
     	@hangman_line = "_" * @guess_limit
     	@guess_count = 0
@@ -17,14 +17,48 @@ class WordGame
  		p @guess_limit
  		p @hangman_line
  		p @guess_count
+ 	end 
+
+ 	def start_game(letter)
+
+ 		p @guess_count
+
+ 		@guess_count =+ 1
+
+ 		p @guess_count
+
+ 		p @letter
 
  	end 
 
 end 
 
-solution = "unicorn"
+# prompt one user to enter a word
+	# store that word 
+	# measure stored words's length 
+		# double the word's length 
+			# store that as the guess limit 
 
-game = WordGame.new("unicorn")
+# user solution-setter 
+
+puts "What word do you want the other person to guess?"
+
+solution = gets.chomp 
+
+game = WordGame.new(solution)
 
 game.print_variables
+
+
+# user solution-guesser 
+
+puts ""
+puts "Alright, hopefully you didn't see that word."
+puts "What letter do you want to guess?"
+
+letter = gets.chomp 
+
+game.start_game(letter)
+
+
 
