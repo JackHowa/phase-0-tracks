b6a67ddf51776ee057a4ff96c15a73a576713fef
jackhowa <johncarverhoward@gmail.com>
2017-03-06 21:19:09 -0600

    Add tests for release0

---
+++ ruby/dancer/dancer.rb
@@ -0,0 +1,14 @@
+class Dancer 
+
+	attr_accessor :name, :age
+
+        def initialize(name,age)
+            @name = name
+            @age = age
+        end
+
+        def pirouette(twirls)
+        	twirls
+        end 
+
+end 
