cb722d9f95ae83cbba85378c4fb985fa1f918f02
jackhowa <johncarverhoward@gmail.com>
2017-03-12 11:47:20 -0500

    Start counter from end

--- js/explore.js
+++ js/explore.js
@@ -3,23 +3,22 @@
 // Make a new program that takes a word 
 // Measure how long that word is 
 // FOR each letter 
-// 		Take the letter's position 
-//		Subtract that letter's position from the word's length 
-// 		Assign that letter to its new position of that result 
+// 		Take the letter's position at the end
+// 		Assign that letter to its new position next in line in front
 // Show the new word 
 
 var stringReverser = function(word) {
 	var new_word = ""
-	for (var i = 0; i < word.length; i++) {
-		letter = word[i];
-		new_word += word[i]
-		console.log(new_word)
-		// console.log(letter);
+	for (var i = word.length - 1; i >= 0; i--) {
+		new_word += word[i];
 	}
+	return new_word
 }
 
+// driver
+
 if(3 > 2) {
-	stringReverser("Jack")
+	console.log(stringReverser("Jack"))
 }
 else {
 	console.log("Error Error Error")
