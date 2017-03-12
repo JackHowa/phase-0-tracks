d64d9fb60bac9deeab9816d83533efbf23a98960
jackhowa <johncarverhoward@gmail.com>
2017-03-12 12:38:28 -0500

    Add push statements to the data structures

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,3 +2,10 @@ var colors = ["blue","read","orange","violet"]
 
 var names = ["Paul","Ringo","John","George"]
 
+names.push("Ed")
+
+console.log(names)
+
+colors.push("yellow")
+
+console.log(colors)
