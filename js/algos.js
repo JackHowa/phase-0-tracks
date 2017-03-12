0b90bf050b072a88fa126123f81845cdc0b531c4
jackhowa <johncarverhoward@gmail.com>
2017-03-12 18:13:15 -0500

    Refactor for clarity later

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,5 @@
+// Release 0
+
 // Set a holder for longest phrase length to zero
 // Set a holder for longest phrase to empty 
 // FOR every word in the phrase list 
@@ -6,26 +8,30 @@
 //			Make that word the the new longest phrase 
 // Show the longest phrase
 
-function longestPhrase(arr) {
+function longestPhrase(phrases) {
 	var longestLength = 0;
 	var longestPhrase = "";
-	for (var i = 0;i < arr.length; i++) {
-		if (arr[i].length > longestLength)
-			longestPhrase = arr[i]
+	for (var i = 0;i < phrases.length; i++) {
+		if (phrases[i].length > longestLength)
+			longestPhrase = phrases[i]
 			longestLength = longestPhrase.length 
 	}
 	return longestPhrase
 }
 
-
-
 // driver code 
-arr = ["long phrase","longest phrase","longer phrase"];
+phrases = ["long phrase","longest phrase","longer phrase"];
 
-console.log(longestPhrase(arr));
+console.log(longestPhrase(phrases));
 
 greetings = ["hello","bonjourno","hola"]
 
 console.log(longestPhrase(greetings))
+
+// Release 1
+
+// function pairMatcher() 
+
+
 
 
