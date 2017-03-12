5d00ec119fa47849716c19c84d822b53fed2b760
jackhowa <johncarverhoward@gmail.com>
2017-03-12 13:16:13 -0500

    Add array interpolate with array and object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,11 +1,29 @@
-var colors = ["blue","read","orange","violet"]
+var colors = ["blue","read","orange","violet"];
 
-var names = ["Paul","Ringo","John","George"]
+var names = ["Paul","Ringo","John","George"];
 
-names.push("Ed")
+// names.push("Ed")
 
-console.log(names)
+// console.log(names)
 
-colors.push("yellow")
+// colors.push("yellow")
 
-console.log(colors)
+// console.log(colors)
+
+var color_names = {};
+
+color_names[names[0]] = colors[0];
+color_names[names[1]] = colors[1];
+color_names[names[2]] = colors[2];
+color_names[names[3]] = colors[3];
+console.log(color_names);
+
+
+// var colorAdder = function(names) {
+// 	for (var i = 0; i < names.length; i++) {
+// 		names.push[colors[i]];
+// 		console.log(names)
+// 	}
+// }
+
+// console.log(colorAdder(names))
