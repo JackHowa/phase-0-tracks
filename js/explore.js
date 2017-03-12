7e2425cf9d57dc006dbf04d8af1053ca0ce3919f
jackhowa <johncarverhoward@gmail.com>
2017-03-11 18:27:22 -0600

    Add pseudocode for reverser function

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// pseudocode 
+
+// Make a new program that takes a word 
+// Measure how long that word is 
+// FOR each letter 
+// 		Take the letter's position 
+//		Subtract that letter's position from the word's length 
+// 		Assign that letter to its new position of that result 
+// Show the new word 
+
