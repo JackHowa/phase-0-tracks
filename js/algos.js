3772ed384d78785a3d2a07174dada67ef12a8d89
jackhowa <johncarverhoward@gmail.com>
2017-03-12 13:58:44 -0500

    Add initial long phrase files and ideas

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+var longestPhrase = function(phrases) {
+	for (var i = 0;i < phrases.length; i++) {
+		console.log(phrases[i].length)
+	}
+}
+
+longestPhrase(["long phrase","longest phrase","longer phrase"])
