1ee59ae4626e9fc1bea298694f179f2968ddcbf1
jackhowa <johncarverhoward@gmail.com>
2017-03-12 21:54:07 -0500

    Add working solution for release 1

--- js/algos.js
+++ js/algos.js
@@ -8,33 +8,49 @@
 //			Make that word the the new longest phrase 
 // Show the longest phrase
 
-function longestPhrase(phrases) {
-	var longestLength = 0;
-	var longestPhrase = "";
-	for (var i = 0;i < phrases.length; i++) {
-		if (phrases[i].length > longestLength)
-			longestPhrase = phrases[i]
-			longestLength = longestPhrase.length 
-	}
-	return longestPhrase
-}
+// function longestPhrase(phrases) {
+// 	var longestLength = 0;
+// 	var longestPhrase = "";
+// 	for (var i = 0;i < phrases.length; i++) {
+// 		if (phrases[i].length > longestLength)
+// 			longestPhrase = phrases[i]
+// 			longestLength = longestPhrase.length 
+// 	}
+// 	return longestPhrase
+// }
 
-// driver code 
-phrases = ["long phrase","longest phrase","longer phrase"];
+// // driver code 
+// phrases = ["long phrase","longest phrase","longer phrase"];
 
-console.log(longestPhrase(phrases));
+// console.log(longestPhrase(phrases));
 
-greetings = ["hello","bonjourno","hola"]
+// greetings = ["hello","bonjourno","hola"]
 
-console.log(longestPhrase(greetings))
+// console.log(longestPhrase(greetings))
 
 // Release 1
 
-function pairMatcher(object1,object2) {
+// Set one state of matching equal to false
+// FOR the first input 
+//		IF first input's attributes match the second's 
+//			then set state of matching equal to true 
+// Show the state of even one of the attributes matching 
 
+function pairMatcher(object1,object2) {
+	for(var key in object1) {
+		if (object1[key] === object2[key]) {
+			return true;
+		}
+	}
+	return false;
 }
 
-object1 = {name: "Steven", age: 54}
-object2 = {name: "Tamir", age: 54}
+object1 = {name: "Steven", age: 54};
+object2 = {name: "Tamir", age: 54};
+object3 = {name: "Jack", age:23};
+
+console.log(pairMatcher(object1,object2))
+
+console.log(pairMatcher(object2,object3))
 
 
