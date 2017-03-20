7b6ee5ede57aed4f425e0ba57807e460335e98ae
jackhowa <johncarverhoward@gmail.com>
2017-03-19 19:31:04 -0500

    Test input device

--- databases/book-catalog/book.rb
+++ databases/book-catalog/book.rb
@@ -15,6 +15,32 @@ create_table_cmd = <<-SQL
   )
 SQL
 
+# create a books table (if it's not there already)
 db.execute(create_table_cmd)
 
-db.execute("select * from books")
+# add a test book
+# db.execute("INSERT INTO books (name, isbn) VALUES ('Hamlet', 9781420922530)")
+
+
+def create_books(db, name, isbn)
+  db.execute("INSERT INTO books (name, isbn) VALUES (?, ?)", [name, isbn])
+end
+
+puts "How many books would you like to input:"
+
+length = gets.chomp
+
+length.to_i.times do
+	puts "Enter book's name:"
+	name = gets.chomp
+	puts "Enter its 13 digit isbn number with no dashes:"
+	isbn = gets.chomp
+  create_books(db, name, isbn)
+end
+
+# explore ORM by retrieving data
+books = db.execute("SELECT * FROM books")
+books.each do |books|
+ puts "#{books['name']} is serial number #{books['isbn']}"
+end
+
