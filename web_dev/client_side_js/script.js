1eb216d3360acb681281e91c8830ef96732411d5
jackhowa <johncarverhoward@gmail.com>
2017-03-21 09:23:01 -0500

    Testing invisibility buttons

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,19 @@
 console.log("The script is running!");
 
+function toggle_visibility(event) {
+       var cham = document.getElementById("lizard-photo");
+   
+       if(cham.style.display == 'block')
+          cham.style.display = 'none';
+       else
+          cham.style.display = 'block';
+}
+
+var cham = document.getElementById("lizard-photo");
+
+var hide = document.getElementById("hide");
+photo.addEventListener("click",toggle_visibility)
+
 function addPinkBorder(event) {
   console.log("click happened! here's the click event:");
   console.log(event);
