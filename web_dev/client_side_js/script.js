9a5153b4dc673574fc0a38694c5267df364f057a
jackhowa <johncarverhoward@gmail.com>
2017-03-21 11:04:56 -0500

    Beautify html

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,13 +1,13 @@
-function hide(event){
-  var monkey = document.getElementById('monkey');
-  monkey.hidden = true;
+function hide(event) {
+    var monkey = document.getElementById('monkey');
+    monkey.hidden = true;
 }
 var hideButton = document.getElementById("hideButton");
 hideButton.addEventListener("click", hide);
 
-function reveal(event){
-  var monkey = document.getElementById('monkey');
-  monkey.hidden = false;
+function reveal(event) {
+    var monkey = document.getElementById('monkey');
+    monkey.hidden = false;
 }
 var revealButton = document.getElementById("revealButton");
 revealButton.addEventListener("click", reveal);
